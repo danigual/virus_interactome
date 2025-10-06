f5255f636f60f7231da880a1b473f34d44954ed5
danigual <danigual40@gmail.com>
2025-10-06 09:24:19 +0200

    Utils module generated

--- virus_interactome/utils.py
+++ virus_interactome/utils.py
@@ -1,5 +1,46 @@
-def test():
-    print('Testeando paquete')
-    print('Testeando paquete 1')
-    print('Testeando paquete 2')
-    print('Testeando paquete 3')
+import json
+from collections import OrderedDict
+
+def load_json(json_path: str):
+    with open (json_path,"r") as j:
+        data = json.load(j)
+    return data
+
+def process_full_data_af3(json_path: str):
+    full_data = load_json(json_path)
+    token_chain_ids = full_data["token_chain_ids"]
+    atom_chain_ids = full_data["atom_chain_ids"] 
+    # pae_matrix = full_data["pae"]
+    
+    chain_lengths = OrderedDict()
+    for chain_id in token_chain_ids:
+        if chain_id not in chain_lengths:
+            chain_lengths[chain_id] = 0
+        chain_lengths[chain_id] += 1
+    
+    atom_chain_lengths = OrderedDict()
+    for atom_id in atom_chain_ids:
+        if atom_id not in atom_chain_lengths:
+            atom_chain_lengths[atom_id] = 0
+        atom_chain_lengths[atom_id] += 1
+
+            
+    chain_boundaries = []
+    start = 0
+    for length in chain_lengths.values():
+        end = start + length
+        chain_boundaries.append((start, end))
+        start = end
+    
+    chain_boundaries_by_atom = []
+    start = 0
+    for length in atom_chain_lengths.values():
+        end = start + length
+        chain_boundaries_by_atom.append((start, end))
+        start = end
+    
+    
+    full_data["chain_lengths"] = chain_lengths 
+    full_data["chain_boundaries"] = chain_boundaries
+    full_data["chain_boundaries_by_atom"] = chain_boundaries_by_atom
+    return full_data
