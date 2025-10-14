e0e0b9c526b02c417a501931b5998151392eb328
PabloHNieto <pablo.h.nieto@gmail.com>
2025-10-14 13:02:20 +0200

    Moving creating af_json to proteome_inputs.py

--- virus_interactome/proteome_utils.py
+++ virus_interactome/proteome_utils.py
@@ -49,54 +49,6 @@ def create_af3_input_json(orf1, orf2, proteome_dict:dict):
             ]
         }
 
-def create_af3_input_json_v2(*args, proteome_dict:dict, proteome_label = None):
-    orf_list = []
-    orf_num_copies = []
-
-    for idx, i in enumerate(args): 
-        if isinstance(i, str):
-            orf_list.append(i)
-        
-            if isinstance(args[idx + 1], int):
-                orf_num_copies.append(args[idx + 1])
-                idx += 2
-            else:
-                orf_num_copies.append(1)
-        
-        else:
-            raise ValueError(f"Expected string at position {i}, got {type(args[i])}")
-    
-    # Check all orfs are in the proteome_dict
-    for orf in orf_list:
-        if orf not in proteome_dict:
-            raise KeyError(f"ORF {orf} is missing from proteome_dict")
-
-    # Check all num_copies are integers and > 0
-    for num in orf_num_copies:
-        if not isinstance(num, int) or num <= 0:
-            raise ValueError(f"Number of copies must be a positive integer, got {num}")
-
-    # Generate the json
-    if proteome_label is not None:
-        proteome_label += "_" 
-    else:
-        proteome_label = ""
-    header = proteome_label + "__".join(orf_list)
-
-    return {
-        "name": header,
-        "sequences": 
-            [
-                {
-                    "proteinChain": {
-                        "count": num,
-                        "sequence": proteome_dict[orf]
-                    }
-                } for orf, num in zip(orf_list, orf_num_copies)
-            ]
-    }
-
-
 def proteome_json (proteome_dict: dict, outputdir:str, batch_size=30):
     '''Receives a dictionary and returns a list of dictionaries as .json file
             ARGS IN:
