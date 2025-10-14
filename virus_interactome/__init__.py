9b41cf92e98cb3b8337251edba0bb6363c8ee9ff
PabloHNieto <pablo.h.nieto@gmail.com>
2025-10-14 12:59:47 +0200

    Updating imports and changes in function names

--- virus_interactome/__init__.py
+++ virus_interactome/__init__.py
@@ -1,5 +1,6 @@
 from .fasta_utils import change_id_proteome
-from .proteome_utils import load_proteome, proteome_json, get_af3_input, process_cif_file, process_interactome, process_boxplot_data
+from .proteome_input import load_proteome
+from .proteome_utils import proteome_json, create_af3_input_json, process_cif_file, process_interactome, process_boxplot_data
 from .plotting import plot_paes, batch_plotting, plot_pLDDT, plot_iptm_vs_ptm, plot_boxplots
 from .utils import load_json, process_full_data_af3
 __all__ = ['change_id_proteome','load_proteome','proteome_json',
